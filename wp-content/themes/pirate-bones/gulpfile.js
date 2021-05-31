3eb191239b50e27319e0875f94f8514e84306410
DeWayne Cameron <firesbane13@hotmail.com>
2021-05-31 21:41:58 +0000

    Adding bones theme and attempting to do stuff

---
+++ wp-content/themes/pirate-bones/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+var paths = {
+    src: "./library/scss",
+    srcFiles: "/**/*.scss",
+    dest: "./library/css/"
+}
+
+gulp.task('styles', () => {
+    return gulp.src(paths.src + paths.srcFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('clean', () => {
+    return del([
+        paths.dest + "*.css",
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
